ae63bc52d83579b1f3a2c10709d1be699cde13cd
JooMin Choi <joominchoi23@gmail.com>
2023-10-02 12:20:31 +0900

    Update basic functions

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages) {
+  this.title = title
+  this.auther = author
+  this.pages = pages
 }
 
 function addBookToLibrary() {
-  // do stuff here
-} 
+  myLibrary.push(book);
+}
