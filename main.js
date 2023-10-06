70333eeb56cacf6c38cb4638407e76ca141e6631
JooMin Choi <joominchoi23@gmail.com>
2023-10-06 15:22:03 +0900

    Add read object to Book function

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
 const myLibrary = ["Book 1", "Book 2", "Book 3", "Book 4", "Book 5", "Book 6"];
 
 const list = document.getElementById("books");
-let newBookbtn = document.querySelector('#new-book-btn');
-
-newBookbtn.addEventListener("click", function() {
-  let newBookForm = document.querySelector("#new-book-form");
-  newBookForm.style.display = "block";
-})
 
 myLibrary.forEach(book => {
   let item = document.createElement("div");
@@ -15,12 +9,19 @@ myLibrary.forEach(book => {
   list.appendChild(item);
 })
 
-function Book(title, author, pages) {
-  this.title = title
-  this.auther = author
-  this.pages = pages
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.auther = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary() {
   myLibrary.push(book);
 }
+
+let newBookbtn = document.querySelector('#new-book-btn');
+newBookbtn.addEventListener("click", function() {
+  let newBookForm = document.querySelector("#new-book-form");
+  newBookForm.style.display = "block";
+})
