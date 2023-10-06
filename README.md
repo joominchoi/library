38a267f47b51280419f0b73230408adbf95d5e84
JooMin Choi <joominchoi23@gmail.com>
2023-10-06 15:32:30 +0900

    Update addBookToLibrary function and add eventlistener to new-book-form

--- README.md
+++ README.md
@@ -9,5 +9,8 @@ Building a small Library app
 
 ## Learnings
 - You can select/deselect CSS properties in inspect to see how it affects the element visually
+- Can put DOM method directly inside a function (such as a querySelector)
+- Use of preventDefault() method to prevent default action (which in our case was called in the eventlistener
+of the submit button to prevent the default behaviour of attempting to send data to a server)
 
 ## Recapping
--- main.js
+++ main.js
@@ -17,11 +17,21 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-  myLibrary.push(book);
+  let title = document.querySelector('#title').value;
+  let author = document.querySelector('#author').value;
+  let pages = document.querySelector('#pages').value;
+  let read = document.querySelector('#read').value;
+  let newBook = new Book(title, author, pages, read)
+  console.log(newBook)
 }
 
 let newBookbtn = document.querySelector('#new-book-btn');
 newBookbtn.addEventListener("click", function() {
   let newBookForm = document.querySelector("#new-book-form");
   newBookForm.style.display = "block";
+})
+
+document.querySelector("#new-book-form").addEventListener("submit", function() {
+  event.preventDefault();
+  addBookToLibrary();
 })
