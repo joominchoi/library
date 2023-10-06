98d8b952d3c6ad634464f13d5690216a6bd2f91b
JooMin Choi <joominchoi23@gmail.com>
2023-10-06 15:19:59 +0900

    Create form for new book and add event listener to button

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const myLibrary = ["Book 1", "Book 2", "Book 3", "Book 4", "Book 5", "Book 6"];
 
 const list = document.getElementById("books");
+let newBookbtn = document.querySelector('#new-book-btn');
+
+newBookbtn.addEventListener("click", function() {
+  let newBookForm = document.querySelector("#new-book-form");
+  newBookForm.style.display = "block";
+})
 
 myLibrary.forEach(book => {
   let item = document.createElement("div");
