56b718ccc458a830be054ef6059c9933688039e3
JooMin Choi <joominchoi23@gmail.com>
2023-10-07 16:20:07 +0900

    Create render function

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const myLibrary = ["Book 1", "Book 2", "Book 3", "Book 4", "Book 5", "Book 6"];
+const myLibrary = [];
 
 const list = document.getElementById("books");
 
@@ -11,9 +11,30 @@ myLibrary.forEach(book => {
 
 function Book(title, author, pages, read) {
   this.title = title;
-  this.auther = author;
+  this.author = author;
   this.pages = pages;
   this.read = read;
+}
+
+function render() {
+  let libraryEl = document.querySelector("#library");
+  libraryEl.innerHTML = ''
+  for (let i = 0; i < myLibrary.length; i++) {
+    let book = myLibrary[i];
+    let bookEl = document.createElement("div");
+    bookEl.innerHTML = 
+    `
+    <div class="card-header">
+      <h3 class="title">${book.title}</h3>
+      <h5 class="author">by ${book.author}</h5>
+    </div>
+    <div class="card-body">
+      <p>${book.pages} pages</p>
+      <p class="read-status">${book.read ? "Read" : "Not Read Yet"}</p>
+    </div>
+    `
+    libraryEl.appendChild(bookEl);
+  }
 }
 
 function addBookToLibrary() {
@@ -22,7 +43,8 @@ function addBookToLibrary() {
   let pages = document.querySelector('#pages').value;
   let read = document.querySelector('#read').value;
   let newBook = new Book(title, author, pages, read)
-  console.log(newBook)
+  myLibrary.push(newBook)
+  render();
 }
 
 let newBookbtn = document.querySelector('#new-book-btn');
