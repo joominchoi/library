53e83da09c58796cce20f3578728245b0dc00779
JooMin Choi <joominchoi23@gmail.com>
2023-10-21 14:43:15 +0900

    Update render function to use DOM methods rather than use innerHTML

--- main.js
+++ main.js
@@ -27,26 +27,81 @@ function toggleRead(index) {
   render();
 }
 
+// function render() {
+//   let libraryEl = document.querySelector("#library");
+//   libraryEl.innerHTML = ''
+//   for (let i = 0; i < myLibrary.length; i++) {
+//     let book = myLibrary[i];
+//     let bookEl = document.createElement("div");
+//     bookEl.className = 'book'
+//     bookEl.innerHTML =
+//       `
+//     <div class="card-header">
+//       <h3 class="title">${book.title}</h3>
+//       <h5 class="author">by ${book.author}</h5>
+//     </div>
+//     <div class="card-body">
+//       <p>${book.pages} pages</p>
+//       <p class="read-status">${book.read ? "Read" : "Not Read Yet"}</p>
+//       <button class="remove-btn" onclick="removeBook(${i})">Remove</button>
+//       <button class=toggle-read-btn" onclick="toggleRead(${i})">Toggle Read</button>
+//     </div>
+//     `
+//     libraryEl.appendChild(bookEl);
+//   }
+// }
+
 function render() {
-  let libraryEl = document.querySelector("#library");
-  libraryEl.innerHTML = ''
+  const libraryEl = document.querySelector("#library");
+  libraryEl.innerHTML = '';
+
   for (let i = 0; i < myLibrary.length; i++) {
-    let book = myLibrary[i];
-    let bookEl = document.createElement("div");
-    bookEl.className = 'book'
-    bookEl.innerHTML =
-      `
-    <div class="card-header">
-      <h3 class="title">${book.title}</h3>
-      <h5 class="author">by ${book.author}</h5>
-    </div>
-    <div class="card-body">
-      <p>${book.pages} pages</p>
-      <p class="read-status">${book.read ? "Read" : "Not Read Yet"}</p>
-      <button class="remove-btn" onclick="removeBook(${i})">Remove</button>
-      <button class=toggle-read-btn" onclick="toggleRead(${i})">Toggle Read</button>
-    </div>
-    `
+    const book = myLibrary[i];
+    const bookEl = document.createElement("div");
+    bookEl.className = 'book';
+
+    const cardHeader = document.createElement("div");
+    cardHeader.className = 'card-header';
+
+    const titleEl = document.createElement("h3");
+    titleEl.className = 'title';
+    titleEl.textContent = book.title;
+
+    const authorEl = document.createElement("h5");
+    authorEl.className = 'author';
+    authorEl.textContent = `by ${book.author}`;
+
+    cardHeader.appendChild(titleEl);
+    cardHeader.appendChild(authorEl);
+
+    const cardBody = document.createElement("div");
+    cardBody.className = 'card-body';
+
+    const pagesEl = document.createElement("p");
+    pagesEl.textContent = `${book.pages} pages`;
+
+    const readStatusEl = document.createElement("p");
+    readStatusEl.className = 'read-status';
+    readStatusEl.textContent = book.read ? "Read" : "Not Read Yet";
+
+    const removeButton = document.createElement("button");
+    removeButton.className = 'remove-btn';
+    removeButton.textContent = 'Remove';
+    removeButton.addEventListener('click', () => removeBook(i));
+
+    const toggleReadButton = document.createElement("button");
+    toggleReadButton.className = 'toggle-read-btn';
+    toggleReadButton.textContent = 'Toggle Read';
+    toggleReadButton.addEventListener('click', () => toggleRead(i));
+
+    cardBody.appendChild(pagesEl);
+    cardBody.appendChild(readStatusEl);
+    cardBody.appendChild(removeButton);
+    cardBody.appendChild(toggleReadButton);
+
+    bookEl.appendChild(cardHeader);
+    bookEl.appendChild(cardBody);
+
     libraryEl.appendChild(bookEl);
   }
 }
