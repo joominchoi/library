db6dec5ccd79c623866596a7a446f7cc736ee9e3
JooMin Choi <joominchoi23@gmail.com>
2023-10-22 20:11:30 +0900

    Update padding of containers and center form

--- main.js
+++ main.js
@@ -2,13 +2,6 @@ const myLibrary = [];
 
 const list = document.getElementById("books");
 
-// function Book(title, author, pages, read) {
-//   this.title = title;
-//   this.author = author;
-//   this.pages = pages;
-//   this.read = read;
-// }
-
 class Book {
   constructor(title, author, pages, read) {
     this.title = title;
@@ -26,30 +19,6 @@ function toggleRead(index) {
   myLibrary[index].toggleRead();
   render();
 }
-
-// function render() {
-//   let libraryEl = document.querySelector("#library");
-//   libraryEl.innerHTML = ''
-//   for (let i = 0; i < myLibrary.length; i++) {
-//     let book = myLibrary[i];
-//     let bookEl = document.createElement("div");
-//     bookEl.className = 'book'
-//     bookEl.innerHTML =
-//       `
-//     <div class="card-header">
-//       <h3 class="title">${book.title}</h3>
-//       <h5 class="author">by ${book.author}</h5>
-//     </div>
-//     <div class="card-body">
-//       <p>${book.pages} pages</p>
-//       <p class="read-status">${book.read ? "Read" : "Not Read Yet"}</p>
-//       <button class="remove-btn" onclick="removeBook(${i})">Remove</button>
-//       <button class=toggle-read-btn" onclick="toggleRead(${i})">Toggle Read</button>
-//     </div>
-//     `
-//     libraryEl.appendChild(bookEl);
-//   }
-// }
 
 function render() {
   const libraryEl = document.querySelector("#library");
